7a6b3bbf7ce84e87a95161b6777ac105db4fa61c
Nguyễn Văn Hải Long <apolokaio2000@gmail.com>
2021-11-13 22:29:49 +0700

    Update search.php

--- project/user/view/search.php
+++ project/user/view/search.php
@@ -22,21 +22,20 @@ if($link === false){
 }
  
 if(isset($_REQUEST["search"])){
-    // Prepare a select statement
+    // Chuẩn bị một câu lệnh SELECT
     $sql = "SELECT * FROM product WHERE name LIKE ?";
     if($stmt = mysqli_prepare($link, $sql)){
-        // Bind variables to the prepared statement as parameters
+        // Liên kết các biến với câu lệnh đã chuẩn bị dưới dạng tham số
         mysqli_stmt_bind_param($stmt, "s", $param_term);
         // Set parameters
 		$search=$_REQUEST["search"];
         $param_term ='%'.$_REQUEST["search"] .'%';
-        // Attempt to execute the prepared statement
+        // Cố gắng thực hiện câu lệnh đã chuẩn bị
         if(mysqli_stmt_execute($stmt)){
             $result = mysqli_stmt_get_result($stmt);
-            
-            // Check number of rows in the result set
+            // Kiểm tra số hàng trong tập kết quả
             if(mysqli_num_rows($result) > 0){
-                // Fetch result rows as an associative array
+                // Tìm nạp các hàng kết quả dưới dạng một mảng kết hợp
                 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
                     echo '<form class="col-md-3">
                             <div class="single-shop-product">
@@ -56,11 +55,11 @@ if(isset($_REQUEST["search"])){
         }
     }
      
-    // Close statement
+    // Đóng câu lệnh
     mysqli_stmt_close($stmt);
 }
  
-// close connection
+// đóng kết nối
 mysqli_close($link);
 ?>
 </div>
